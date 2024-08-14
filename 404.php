8a8827c7d46199342614f82c55275da46a537230
Anupam Mondal <a09051985@gmail.com>
2024-08-14 09:07:43 +0530

    404 added

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <header class="page-header">
+            <h1 class="page-title"><?php _e( 'Oops! That page can’t be found.', 'theme-text-domain' ); ?></h1>
+        </header>
+        <div class="page-content">
+            <p><?php _e( 'It looks like nothing was found at this location. Maybe
