b093f930b596ace07bb5554e6d158b1dbb284450
Anupam Mondal <a09051985@gmail.com>
2024-08-19 13:15:55 +0530

    Update js file to function

--- functions.php
+++ functions.php
@@ -3,3 +3,8 @@ function myclassictheme_enqueue_styles() {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'myclassictheme_enqueue_styles' );
+
+function my_custom_scripts() {
+    wp_enqueue_script('custom-js', get_template_directory_uri() . '/custom.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'my_custom_scripts');
