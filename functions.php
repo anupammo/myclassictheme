e2bde09c8f1619b2b83866905feca3b349a8635f
Anupam Mondal <a09051985@gmail.com>
2024-08-19 13:37:17 +0530

    190820240137

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 function myclassictheme_enqueue_styles() {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
@@ -8,3 +8,13 @@ function my_custom_scripts() {
     wp_enqueue_script('custom-js', get_template_directory_uri() . '/custom.js', array(), null, true);
 }
 add_action('wp_enqueue_scripts', 'my_custom_scripts');
+
+function register_my_menus() {
+    register_nav_menus(
+        array(
+            'primary' => __('Primary Menu'),
+            'mobile' => __('Mobile Menu')
+        )
+    );
+}
+add_action('init', 'register_my_menus');
